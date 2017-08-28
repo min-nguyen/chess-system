219bbe15aca36258270b9f2fc6447a7abcf9d489
Minh <minhnguyen1995@googlemail.com>
2017-08-28 16:46:20 +0100

    char.cpp

--- Game/Char.h
+++ Game/Char.h
@@ -13,14 +13,16 @@ namespace CharState {
 
 class Char {
 public:
-    Char(std::string fileName);
+    Char(std::string fileName, sf::RenderWindow window);
     void debug(std::string s);
     sf::Sprite getCurrentSprite();
     void update(sf::Time elapsedTime);
     void updateState(CharState::STATE st);
+    void draw();
     std::pair<float, float> position;
 protected:
     SpriteSheet spriteSheet;
+    sf::RenderWindow window;
     std::vector<sf::Sprite*> idleSprites;
     std::vector<sf::Sprite*> walkSprites;
     std::vector<sf::Sprite*>::iterator it;
